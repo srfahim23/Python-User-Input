e77f3d514519db2e6361d23aca270b89738318f9
Muhammad Faysal <srfahim23@gmail.com>
2023-02-17 02:39:44 +0600

    this-update

---
+++ main.py
@@ -0,0 +1,11 @@
+#Python-User-Input
+
+# Python 3.6
+# username = input("Enter username:")
+# print("Username is: " + username)
+# It's just using for my computer
+
+# Python 2.7
+# username = raw_input("Enter username:")
+# print("Username is: " + username)
+# This is just for python 2.7 version
