1263577de221ca5a6cef3075da3d7f6834e19682
Muhammad Faysal <srfahim23@gmail.com>
2023-02-24 21:30:48 +0600

    this-push

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-#Python-User-Input
+I am just cheking branc have or not 
 
-# Python 3.6
-# username = input("Enter username:")
-# print("Username is: " + username)
-# It's just using for my computer
-
-# Python 2.7
-# username = raw_input("Enter username:")
-# print("Username is: " + username)
-# This is just for python 2.7 version
